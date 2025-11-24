19f1e5475952b89cdb0cde8fdc53f0088d2fd247
crd-codes <dugadechakradhar@gmail.com>
2025-11-24 09:50:53 +0530

    Assignment_31 programs added

---
+++ Assignments/Assignment_31/Program31_1.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        if(iRow != iCol)
+        {
+            System.out.println("Invalid input");
+            System.out.println("Row number and column number should be same");           
+            
+            return;
+        }
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; i >= j; j++)
+            {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Program31_1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter the number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        
+        pobj.Display(iValue1,iValue2);
+    }
+}
